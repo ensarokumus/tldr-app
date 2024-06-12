192b3c7c691fe133833c9866dc833dd2a7b14ced
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-12 14:34:20 +1000

    feat: adds kindeAuth with Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react/";
 
 const Navbar = () => {
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href={"/"} className="flex z-40 font-bold text-xl">
+          <Link href={"/"} className="flex z-40 font-bold text-xl text-black">
             TL
             <span className="text-primary font-extrabold leading-5 text-2xl mx-0.5">
               ;
@@ -28,6 +30,21 @@ const Navbar = () => {
               >
                 Pricing
               </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Sign in
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: "sm",
+                })}
+              >
+                Sign up <ArrowRight className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
             </>
           </div>
         </div>
