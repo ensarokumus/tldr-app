e9d7763de88c4282265baaf29b4728d820ce2738
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-13 23:19:38 +1000

    feat: adds UploadButton component modal

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(o) => {
+        if (!o) {
+          setIsOpen(o);
+        }
+      }}
+    >
+      <DialogTrigger asChild onClick={() => setIsOpen(true)}>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+
+      <DialogContent>example content</DialogContent>
+    </Dialog>
+  );
+};
+export default UploadButton;
