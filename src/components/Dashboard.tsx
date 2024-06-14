9d2364d70dc692348f30528e6ad42411350df284
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-14 10:23:23 +1000

    feat: adds my files containers to Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,8 +2,11 @@
 
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
-import { Ghost } from "lucide-react";
+import { Ghost, MessageSquare, Plus, TrashIcon } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+import { format } from "date-fns";
+import { Button } from "./ui/button";
 
 const Dashboard = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
@@ -16,10 +19,58 @@ const Dashboard = () => {
       </div>
 
       {files && files?.length !== 0 ? (
-        // add files here
-        <div></div>
+        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg-grid-cols-3">
+          {files
+            .sort(
+              (a, b) =>
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+            )
+            .map((file) => (
+              <li
+                key={file.id}
+                className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"
+              >
+                <Link
+                  href={`/dashboard/${file.id}`}
+                  className="flex flex-col gap-2"
+                >
+                  <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
+                    <div
+                      aria-hidden
+                      className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-primary to-secondary"
+                    ></div>
+                    <div className="flex-1 truncate">
+                      <div className="flex items-center space-x-3">
+                        <h3 className="truncate text-lg font-medium text-zinc-900">
+                          {file.name}
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+
+                <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-900">
+                  <div className="flex items-center gap-2">
+                    <Plus className="h-4 w-4" />
+                    {format(new Date(file.createdAt), "MMM yyyy")}
+                  </div>
+
+                  <div className="flex items-center gap-2">
+                    <MessageSquare className="h-4 w-4" />
+                    {/* display how many messages use has exchanged with the PDF file */}
+                    MessageCount (TBA)
+                  </div>
+
+                  <Button size="sm" className="w-full" variant="destructive">
+                    <TrashIcon className="h-4 w-4" />
+                  </Button>
+                </div>
+              </li>
+            ))}
+        </ul>
       ) : isLoading ? (
-        <Skeleton height={100} className="my-2" count={3}/>
+        <Skeleton height={100} className="my-2" count={3} />
       ) : (
         <div className="mt-16 flex flex-col items-center gap-2">
           <Ghost className="h-8 w-8 text-zinc-800" />
