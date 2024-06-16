ec49fb7a2d88e79441d0ce7b40c73d586284b8a4
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-17 09:37:36 +1000

    feat: adds react-resize-detector to resize pdfview

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -6,6 +6,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { useToast } from "./ui/use-toast";
+import { useResizeDetector } from "react-resize-detector";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -19,6 +20,8 @@ interface PdfRendererProps {
 const PdfRenderer = ({ url }: PdfRendererProps) => {
   const { toast } = useToast();
 
+  const { width, ref } = useResizeDetector();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -26,7 +29,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        <div>
+        <div ref={ref}>
           <Document
             loading={
               <div className="flex justify-center">
@@ -43,7 +46,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             file={url}
             className="max-h-full"
           >
-            <Page pageNumber={1} />
+            <Page width={width ? width : 1} pageNumber={1} />
           </Document>
         </div>
       </div>
