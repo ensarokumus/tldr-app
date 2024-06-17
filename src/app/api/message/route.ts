d1ebb4a135eeea09c98fb257109e0bd79b7480b8
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-17 16:04:09 +1000

    feat: adds POST req endpoint for sending messages

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types";
+import { NextRequest } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  // endpoint for asking a question to a pdf file
+
+  const body = await request.json();
+
+  const { getUser } = getKindeServerSession();
+  const user: KindeUser | null = await getUser();
+
+  const { id: userId } = user || {};
+
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+
+  if (!file) {
+    return new Response("Npt found", { status: 404 });
+  }
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId,
+    },
+  });
+};
