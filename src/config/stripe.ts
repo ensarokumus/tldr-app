fe62b59a09c5416e2da85abff322a91d10e9ebab
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-20 15:03:14 +1000

    feat: adds stripe config file

---
+++ src/config/stripe.ts
@@ -0,0 +1,30 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    fileSizePerPdf: 4,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    fileSizePerPdf: 16,
+    price: {
+      amount: 2,
+      priceIds: {
+        test: "price_1PTcJo06efvHn3KqIpFF3sry",
+        production: "",
+      },
+    },
+  },
+];
