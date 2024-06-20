10ecc4ed803af5f701bbad76e60c1494e50fc6c5
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-20 15:38:13 +1000

    feat: connects stripe payment provider

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import { ArrowRight } from "lucide-react";
 import { Button } from "./ui/button";
+import { trpc } from "@/app/_trpc/client";
 
 const UpgradeButton = () => {
+  const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      window.location.href = url ?? "/dashboard/billing";
+    },
+  });
+
   return (
-    <Button className="w-full">
+    <Button onClick={() => createStripeSession()} className="w-full">
       Upgrade now <ArrowRight className="h-5 w-5 ml-1.5" />
     </Button>
   );
