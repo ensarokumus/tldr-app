09237f729e3d196361490a1a3e0ead90ab2b7dae
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-21 11:18:25 +1000

    feat: adds Suspense to auth-callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,8 +3,9 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
-const Page = () => {
+const AuthCallbackContent = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
@@ -36,6 +37,26 @@ const Page = () => {
   }
 
   return null;
+};
+
+const Page = () => {
+  return (
+    <Suspense
+      fallback={
+        <div className="w-full mt-24 flex justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+            <h3 className="font-semibold text-xl">
+              Setting up your account...
+            </h3>
+            <p>You will be redirected automatically.</p>
+          </div>
+        </div>
+      }
+    >
+      <AuthCallbackContent />
+    </Suspense>
+  );
 };
 
 export default Page;
